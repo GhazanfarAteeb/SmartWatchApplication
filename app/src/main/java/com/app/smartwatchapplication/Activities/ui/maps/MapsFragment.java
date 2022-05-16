234f7033732cce20594d57b649ed5c9d93d5906f
Ghazanfar Ateeb <strange.person9125@gmail.com>
2022-05-16 19:14:13 +0500

    integrated maps fragment

--- app/src/main/java/com/app/smartwatchapplication/Activities/ui/maps/MapsFragment.java
+++ app/src/main/java/com/app/smartwatchapplication/Activities/ui/maps/MapsFragment.java
@@ -6,22 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.app.smartwatchapplication.R;
 import com.app.smartwatchapplication.databinding.FragmentMapsBinding;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private FragmentMapsBinding binding;
-
+    public static GoogleMap map;
     View root;
+    SupportMapFragment mapsFragment;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentMapsBinding.inflate(inflater, container, false);
         root = binding.getRoot();
-
+        mapsFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.maps);
         return root;
     }
 
@@ -32,6 +36,15 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setAllGesturesEnabled(false);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (mapsFragment !=null) {
+            mapsFragment.getMapAsync(this);
+        }
     }
 }
