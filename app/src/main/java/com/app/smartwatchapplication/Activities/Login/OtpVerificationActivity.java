f8ac003ec7f2a46ab2183d4939bdc41a75738c26
Ghazanfar Ateeb <strange.person9125@gmail.com>
2022-05-16 10:43:29 +0500

    WATCH READING DONE

--- app/src/main/java/com/app/smartwatchapplication/Activities/Login/OtpVerificationActivity.java
+++ app/src/main/java/com/app/smartwatchapplication/Activities/Login/OtpVerificationActivity.java
@@ -2,8 +2,10 @@ package com.app.smartwatchapplication.Activities.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.app.smartwatchapplication.Activities.WatchScanActivity;
 import com.app.smartwatchapplication.R;
 
 public class OtpVerificationActivity extends AppCompatActivity {
@@ -12,5 +14,9 @@ public class OtpVerificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verification);
+
+        findViewById(R.id.btn_verify_otp).setOnClickListener(view -> {
+            startActivity(new Intent(OtpVerificationActivity.this, WatchScanActivity.class));
+        });
     }
 }
--- app/src/main/java/com/app/smartwatchapplication/Activities/WatchScanActivity.java
+++ app/src/main/java/com/app/smartwatchapplication/Activities/WatchScanActivity.java
@@ -1,6 +1,7 @@
 package com.app.smartwatchapplication.Activities;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +10,8 @@ import com.app.smartwatchapplication.R;
 import com.htsmart.wristband2.WristbandApplication;
 import com.htsmart.wristband2.WristbandManager;
 import com.htsmart.wristband2.bean.ConnectionState;
+import com.htsmart.wristband2.bean.HealthyDataResult;
+import com.htsmart.wristband2.bean.WristbandConfig;
 import com.polidea.rxandroidble2.RxBleClient;
 import com.polidea.rxandroidble2.RxBleDevice;
 import com.polidea.rxandroidble2.scan.ScanSettings;
@@ -16,7 +19,10 @@ import com.polidea.rxandroidble2.scan.ScanSettings;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class WatchScanActivity extends AppCompatActivity {
     WristbandManager wristbandManager = WristbandApplication.getWristbandManager();
@@ -59,6 +65,51 @@ public class WatchScanActivity extends AppCompatActivity {
                             wristbandManager.connect(scanResult.getBleDevice(), "1", true, true, 30, (float) 165.5, (float) 70.8);
                             System.out.println("Connected to" + scanResult.getBleDevice().getMacAddress());
                             System.out.println("CONNECTION_STATE: " + wristbandManager.isConnected());
+                            wristbandManager.observerConnectionState().subscribe(new Observer<ConnectionState>() {
+                                @Override
+                                public void onSubscribe(Disposable d) {
+                                    System.out.println(d.isDisposed());
+                                }
+
+                                @Override
+                                public void onNext(ConnectionState connectionState) {
+                                    if (connectionState.toString().equals("CONNECTED")) {
+                                        WristbandConfig wristbandConfig = wristbandManager.getWristbandConfig();
+                                        int healthType = 0;
+                                        System.out.println(wristbandManager.isConnected());
+                                        healthType|= WristbandManager.HEALTHY_TYPE_HEART_RATE;
+                                        healthType|= WristbandManager.HEALTHY_TYPE_BLOOD_PRESSURE;
+                                        healthType|= WristbandManager.HEALTHY_TYPE_OXYGEN;
+                                        healthType|= WristbandManager.HEALTHY_TYPE_RESPIRATORY_RATE;
+                                        Disposable healthSystem =  wristbandManager.openHealthyRealTimeData(healthType, Integer.MAX_VALUE).
+                                                observeOn(AndroidSchedulers.mainThread())
+                                                .subscribe(new Consumer<HealthyDataResult>() {
+                                                    @Override
+                                                    public void accept(HealthyDataResult healthyDataResult) throws Exception {
+                                                        Log.d("HEART_RATE", String.valueOf(healthyDataResult.getHeartRate()));
+                                                        Log.d("BP_RATE", ""+ healthyDataResult.getSystolicPressure()+"/"+healthyDataResult.getDiastolicPressure());
+                                                        Log.d("SPO2_RATE", String.valueOf(healthyDataResult.getOxygen()));
+                                                        Log.d("RESPIRATION_RATE", String.valueOf(healthyDataResult.getRespiratoryRate()));
+                                                    }
+                                                }, new Consumer<Throwable>() {
+                                                    @Override
+                                                    public void accept(Throwable throwable) throws Exception {
+                                                        throwable.getLocalizedMessage();
+                                                    }
+                                                });
+                                    }
+                                }
+
+                                @Override
+                                public void onError(Throwable e) {
+                                    e.getLocalizedMessage();
+                                }
+
+                                @Override
+                                public void onComplete() {
+                                    System.out.println("CONNECTED SUCCESSFULLY");
+                                }
+                            });
                         }
                     }
                 }, Throwable::getLocalizedMessage);
