070f43749a24cc6a3d02e3982d2319fb3622c512
usama <dmupster>
2022-05-23 14:40:18 +0500

    initial commit

--- app/src/main/java/com/app/smartwatchapplication/Activities/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/app/smartwatchapplication/Activities/ui/profile/ProfileFragment.java
@@ -4,70 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.app.smartwatchapplication.AppConstants.Constants;
-import com.app.smartwatchapplication.R;
 import com.app.smartwatchapplication.databinding.FragmentProfileBinding;
-import com.google.android.material.textfield.TextInputEditText;
+import com.htsmart.wristband2.WristbandApplication;
+import com.htsmart.wristband2.WristbandManager;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
-    TextInputEditText tieName, tieAddress, tiePhoneNumber, tieLicenseNumber,
-            tieDateOfJoining, tieVehicleName, tieManufacturer, tieEngineNumber,
-            tieChassisNumber, tieVehicleType;
-    Button btnLogout;
-    View view;
-
+    WristbandManager wristbandManager = WristbandApplication.getWristbandManager();
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-        view = binding.getRoot();
-        return view;
-    }
+        View root = binding.getRoot();
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        init();
-        setData();
-        setListeners();
+        final TextView textView = binding.textNotifications;
+        return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    private void init() {
-        tieName = view.findViewById(R.id.tie_name);
-        tieAddress = view.findViewById(R.id.tie_address);
-        tiePhoneNumber = view.findViewById(R.id.tie_phone_no);
-        tieEngineNumber = view.findViewById(R.id.tie_engine_no);
-        tieDateOfJoining = view.findViewById(R.id.tie_doj);
-        tieChassisNumber = view.findViewById(R.id.tie_chassis_no);
-        tieLicenseNumber = view.findViewById(R.id.tie_license_no);
-        tieManufacturer = view.findViewById(R.id.tie_manufacturer_name);
-        tieVehicleName = view.findViewById(R.id.tie_vehicle_name);
-        tieVehicleType = view.findViewById(R.id.tie_vehicle_type);
-        btnLogout = view.findViewById(R.id.btn_logout);
-    }
-
-    private void setListeners() {
-        btnLogout.setOnClickListener(view1 -> {
-
-        });
-    }
-
-    private void setData() {
-        tieName.setText(Constants.USER.getUser().get(0).getdName());
-        tieAddress.setText(Constants.USER.getUser().get(0).getdAddress());
-        tiePhoneNumber.setText(Constants.USER.getUser().get(0).getdMobile());
     }
 }
