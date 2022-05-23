a9775ea81d542dea5a64c442d3b84913cf7e4029
Ghazanfar Ateeb <strange.person9125@gmail.com>
2022-05-23 13:36:33 +0500

    LAYOUT PROFILE FRAGMENT ISSUES RESOLVED AND IDS ADDED

--- app/src/main/java/com/app/smartwatchapplication/Activities/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/app/smartwatchapplication/Activities/ui/profile/ProfileFragment.java
@@ -4,29 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.app.smartwatchapplication.databinding.FragmentProfileBinding;
-import com.htsmart.wristband2.WristbandApplication;
-import com.htsmart.wristband2.WristbandManager;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
-    WristbandManager wristbandManager = WristbandApplication.getWristbandManager();
+    TextInputEditText tieName, tieAddress, tiePhoneNumber, tieLicenseNumber,
+            tieDateOfJoining, tieVehicleName, tieManufacturer, tieEngineNumber,
+            tieChasisNumber, tieVehicleType;
+    View view;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        view = binding.getRoot();
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void init() {
+
     }
 }
